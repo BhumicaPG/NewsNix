668799e17b930850b3c857c626ba1996f686d200
BhumicaPG <bhumigangurde8@gmail.com>
2023-12-27 21:54:22 +0530

    Updates

---
+++ precache-manifest.bffe702c9717c6fad0d8ddb85cd0462d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03772c7309e8193cd9b5c83bb128df20",
+    "url": "/NewsAPI/index.html"
+  },
+  {
+    "revision": "14ca4232bccfac83f34e",
+    "url": "/NewsAPI/static/css/3.4c4477be.chunk.css"
+  },
+  {
+    "revision": "72d6f5cba83025ad1b13",
+    "url": "/NewsAPI/static/css/main.4e8c31a5.chunk.css"
+  },
+  {
+    "revision": "a38b76b501c3a99390a8",
+    "url": "/NewsAPI/static/js/0.afd3638a.chunk.js"
+  },
+  {
+    "revision": "e2a608f3b392ed4f1f89ddb34b2dbcda",
+    "url": "/NewsAPI/static/js/0.afd3638a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14ca4232bccfac83f34e",
+    "url": "/NewsAPI/static/js/3.82ed4c13.chunk.js"
+  },
+  {
+    "revision": "cbc41762b08bce4213514217ce9f16f9",
+    "url": "/NewsAPI/static/js/3.82ed4c13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3461aa28d4535d00df74",
+    "url": "/NewsAPI/static/js/4.a7082158.chunk.js"
+  },
+  {
+    "revision": "599cda7d0c48bfafd3d7",
+    "url": "/NewsAPI/static/js/5.a10303fa.chunk.js"
+  },
+  {
+    "revision": "9fdb1b535bdb4b8129c8",
+    "url": "/NewsAPI/static/js/6.b9ccb172.chunk.js"
+  },
+  {
+    "revision": "72d6f5cba83025ad1b13",
+    "url": "/NewsAPI/static/js/main.09317925.chunk.js"
+  },
+  {
+    "revision": "d6fe36acca11111e5258",
+    "url": "/NewsAPI/static/js/runtime-main.7c7ebe07.js"
+  }
+]);
